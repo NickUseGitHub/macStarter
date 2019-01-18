0f0c2fbe6faab34ab6ff214a8c8631737302c593
nicknoi Tencent <chaiwat@tencent.co.th>
2019-01-18 17:35:23 +0700

    [master] create script install npm & node

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
 
 #install powerlevel9k
-brew install powerlevel9k
+&& brew install powerlevel9k \
+
+#install node & npm
+&& brew install npm \
+&& brew install node
