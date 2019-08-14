12ff70ae64a43f96d1c23daa22fc4168d36dec93
Nick Coder <nickt.tnick@gmail.com>
2019-08-14 18:25:43 +0700

    change script shell and remove power9k

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
 
-#install powerlevel9k
-&& brew install powerlevel9k \
-
 #install node & npm
-&& brew install npm \
-&& brew install node
+brew install npm
+brew install node
+
+#install powerlevel9k
+brew install zsh zsh-completions
